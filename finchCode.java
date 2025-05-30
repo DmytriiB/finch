67006675dcc760c448677ee758cfa2070f37d66d
DmytriiB <dmytriib@nycstudents.net>
2025-05-30 10:04:36 -0400

    Update finchCode.java

--- finchCode.java
+++ finchCode.java
@@ -6,11 +6,11 @@ public class TestingFinch{
     int right = myFinch.getRightObstacleSensor();
 
     if(left > 0 || right > 0){
-      setMotors(0.0,0.0);
-      setTurn("R",90,100);
-      setMotors(100.0,100.0);
+      f.setMotors(0.0,0.0);
+      f.setTurn("R",90,100);
+      f.setMotors(100.0,100.0);
     }
     else{
-      setMotors(100.0,100.0);
+      f.setMotors(100.0,100.0);
     }
     
